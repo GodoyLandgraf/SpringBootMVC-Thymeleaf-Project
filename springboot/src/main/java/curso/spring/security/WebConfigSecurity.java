20e368995f377f1cb1734c5473498514292d430d
GodoyLandgraf <matheus_godoyland@hotmail.com>
2022-07-20 09:30:12 -0300

    validação em memória usando SpringSecurity

---
+++ springboot/src/main/java/curso/spring/security/WebConfigSecurity.java
@@ -0,0 +1,42 @@
+package curso.spring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+	//Configura as solicitações de acesso por Http
+	@Override 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable() //Desativa as configurações padrão de memória
+		.authorizeRequests() //Permitir restringir acessos
+		.antMatchers(HttpMethod.GET, "/").permitAll() //qualquer usuário acessa a página principal
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll() //permite qualquer usuário
+		.and().logout() //Mapeia URL de logout e invalida usuario autenticado
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override //cria autenticação do usuário com banco de dados ou em memória
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+		.withUser("Admin")
+		.password("admin123")
+		.roles("ADMIN");
+	}
+	
+	@Override //Ignora URL especificas
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/materialize/**");
+	}
+}
