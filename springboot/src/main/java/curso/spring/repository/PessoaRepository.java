243c0d8d4903adf9f7a520f08fcb5636de0b1552
GodoyLandgraf <matheus_godoyland@hotmail.com>
2022-07-27 12:29:10 -0300

    Adicionando relatório de pessoas cadastradas para download com Jasper.

--- springboot/src/main/java/curso/spring/repository/PessoaRepository.java
+++ springboot/src/main/java/curso/spring/repository/PessoaRepository.java
@@ -17,5 +17,8 @@ public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 	List<Pessoa> findPessoaByName(String nome);
 	
 	@Query("select p from Pessoa p where p.nome like %?1% and p.sexopessoa = ?2")
-	List<Pessoa> findPessoaBySexo(String nome, String sexopessoa );
+	List<Pessoa> findPessoaByNameSexo(String nome, String sexopessoa );
+	
+	@Query("select p from Pessoa p where p.sexopessoa = ?1 ")
+	List<Pessoa> findPessoaBySexo(String sexo);
 }
